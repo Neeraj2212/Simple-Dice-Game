286fa1958e57e180d690d4b4b8b07661d5b24408
Neeraj Patil <55763684+Neeraj2212@users.noreply.github.com>
2020-08-11 22:19:53 +0530

    Button Added

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-var num1 = Math.random();
-num1 = Math.floor(num1 * 6) + 1;
+document.querySelector('.btn').addEventListener('click', function () {
+	var num1 = Math.random();
+	num1 = Math.floor(num1 * 6) + 1;
 
-var num2 = Math.random();
-num2 = Math.floor(num2 * 6) + 1;
+	var num2 = Math.random();
+	num2 = Math.floor(num2 * 6) + 1;
+	document
+		.querySelector('.img1')
+		.setAttribute('src', 'images/dice' + String(num1) + '.png');
 
-document
-	.querySelector('.img1')
-	.setAttribute('src', 'images/dice' + String(num1) + '.png');
+	document
+		.querySelector('.img2')
+		.setAttribute('src', 'images/dice' + String(num2) + '.png');
 
-document
-	.querySelector('.img2')
-	.setAttribute('src', 'images/dice' + String(num2) + '.png');
-
-if (num1 === num2) {
-	document.querySelector('h1').innerHTML = '🚩Draw🚩';
-} else if (num1 >= num2) {
-	document.querySelector('h1').innerHTML = '🚩Player1 Wins';
-} else {
-	document.querySelector('h1').innerHTML = 'Player2 Wins🚩';
-}
+	if (num1 === num2) {
+		document.querySelector('h1').innerHTML = '🚩Draw🚩';
+	} else if (num1 >= num2) {
+		document.querySelector('h1').innerHTML = '🚩Player1 Wins';
+	} else {
+		document.querySelector('h1').innerHTML = 'Player2 Wins🚩';
+	}
+});
